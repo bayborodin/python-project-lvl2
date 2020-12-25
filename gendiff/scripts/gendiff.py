7178fc937a9ac1c45dd7dc061204cbf07529bcc1
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2020-12-25 23:06:29 +1000

    Fix the hexlet action

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 """A gendiff utility startup script."""
 import argparse
+import pkg_resources
 
 from gendiff import generate_diff
 from gendiff.formatters.formatter import DEFAULT_FORMAT, FORMATS
@@ -11,6 +12,10 @@ def main():
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
+
+    app_version = pkg_resources.get_distribution('gendiff').version
+    parser.add_argument('-V', '--version', action='version',
+                        version=app_version)
 
     args = parser.parse_args()
     output_format = args.format
